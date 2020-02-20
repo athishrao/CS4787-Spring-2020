bfed72357fc126f81d367d29e6659dce6081f45f
Athish Rahul Rao <athish@dhcp-hol-6517.eduroam.cornell.edu>
2020-02-19 20:10:10 -0500

    V9

--- PA1/main.py
+++ PA1/main.py
@@ -112,7 +112,7 @@ def test_gradient(Xs, Ys, gamma, W, alpha):
 def multinomial_logreg_error(Xs, Ys, W):
     # TODO students should implement this
     Ys = Ys.T
-    yHat = softmax(np.dot(W,Xs),).T
+    yHat = softmax(np.dot(W,Xs),axis=0).T
     count = 0
     for i in range(len(Ys)):
         pred = np.argmax(yHat[i])
@@ -217,11 +217,10 @@ def gradient_descent(Xs, Ys, gamma, W0, alpha, num_iters, monitor_freq, starter=
 # returns   the estimated model error when sampling with replacement
 def estimate_multinomial_logreg_error(Xs, Ys, W, nsamples):
     # TODO students should implement this
-    # X_sub, Y_sub = Xs.T, Ys.T
-    perm = permutation(Xs.shape[1])
+    X_sub, Y_sub = Xs.T, Ys.T
+    perm = [np.random.randint(0,Xs.shape[1]) for _ in range(nsamples) ]
     X_sub = Xs.T[perm]
     Y_sub = Ys.T[perm]
-    X_sub, Y_sub = X_sub[:nsamples], Y_sub[:nsamples]
     estimated_error = multinomial_logreg_error(X_sub.T, Y_sub.T, W)
     return estimated_error
 
@@ -231,7 +230,7 @@ if __name__ == "__main__":
     Xs_tr, Xs_te, Ys_tr, Ys_te = np.matrix(Xs_tr), np.matrix(Xs_te), np.matrix(Ys_tr), np.matrix(Ys_te)
     print("Shape of initial X:", Xs_tr.shape)
     print("Shape of initial Y:", Ys_tr.shape)
-
+    
     DIVIDER = "#"*20
 
     # Part 1
@@ -243,7 +242,7 @@ if __name__ == "__main__":
         ret = test_gradient(Xs_tr, Ys_tr, gamma, W, a)
         print(f"For alpha={a}, average difference is: {ret}")
     print("\nPart 1 complete .\n")
-
+    
 
     # Part 2
     print(f"{DIVIDER}\nRunning part 2 ...\n")
@@ -252,7 +251,7 @@ if __name__ == "__main__":
     numberIter = 10
     monitorFreq = 10
     W = np.zeros([Ys_tr.shape[0], Xs_tr.shape[0]])
-
+    
     print(f"Running starter code implementation config: alpha={alpha}, gamma={gamma}, #iterations={numberIter}, monitorFreq={monitorFreq}")
     start = datetime.datetime.now()
     Ws_starter = gradient_descent(Xs_tr, Ys_tr, gamma, W, alpha, numberIter, monitorFreq, True)
@@ -263,7 +262,7 @@ if __name__ == "__main__":
     print(f"{DIVIDER}\nRunning part 3 ...\n")
     W = np.zeros([Ys_tr.shape[0], Xs_tr.shape[0]])
     end = datetime.datetime.now()
-
+    
     print(f"Running numpy implementation config: alpha={alpha}, gamma={gamma}, #iterations={numberIter}, monitorFreq={monitorFreq}")
     start = datetime.datetime.now()
     Ws_numpy = gradient_descent(Xs_tr, Ys_tr, gamma, W, alpha, numberIter, monitorFreq)
@@ -288,29 +287,39 @@ if __name__ == "__main__":
     end = datetime.datetime.now()
     print("\nRunning time to obtain training error with entire dataset on a single model is:", end - start)
 
+    start = datetime.datetime.now()
+    _ = estimate_multinomial_logreg_error(Xs_tr, Ys_tr, Ws_numpy[-1], 100)
+    end = datetime.datetime.now()
+    print("\nRunning time to obtain training error with subsampling 100 samples on a single model is:", end - start)
 
+    start = datetime.datetime.now()
+    _ = estimate_multinomial_logreg_error(Xs_tr, Ys_tr, Ws_numpy[-1], 1000)
+    end = datetime.datetime.now()
+    print("\nRunning time to obtain training error with subsampling 1000 samples on a single model is:", end - start)
+
+    
     for w in Ws_numpy:
 
         loss.append(multinomial_logreg_total_loss(Xs_tr, Ys_tr, gamma, w))
         loss_np_te += [multinomial_logreg_total_loss(Xs_te, Ys_te, gamma, w)]
-
+        
         error.append(multinomial_logreg_error(Xs_tr, Ys_tr, w))
         error_np_te += [multinomial_logreg_error(Xs_te, Ys_te, w)]
-
+        
         num_ex = 100
         est_err_tr_100.append(estimate_multinomial_logreg_error(Xs_tr, Ys_tr, w, num_ex))
         est_err_te_100.append(estimate_multinomial_logreg_error(Xs_te, Ys_te, w, num_ex))
-
+        
         num_ex = 1000
         est_err_tr_1000.append(estimate_multinomial_logreg_error(Xs_tr, Ys_tr, w, num_ex))
         est_err_te_1000.append(estimate_multinomial_logreg_error(Xs_te, Ys_te, w, num_ex))
-
+        
 
 
     plt.plot(range(numberIter//monitorFreq+1), loss_np_te)
     plt.savefig("results/entire_ds_loss_test_"+str(numberIter)+".png")
     plt.close()
-
+    
     plt.plot(range(numberIter//monitorFreq+1), error_np_te)
     plt.savefig("results/entire_ds_error_test_"+str(numberIter)+".png")
     plt.close()
@@ -326,7 +335,7 @@ if __name__ == "__main__":
     plt.plot(range(numberIter//monitorFreq+1), est_err_tr_100)
     plt.savefig("results/subsample_100_estimated_err_train_"+str(numberIter)+".png")
     plt.close()
-
+    
     plt.plot(range(numberIter//monitorFreq+1), est_err_tr_1000)
     plt.savefig("results/subsample_1000_estimated_err_train_"+str(numberIter)+".png")
     plt.close()
@@ -334,7 +343,7 @@ if __name__ == "__main__":
     plt.plot(range(numberIter//monitorFreq+1), est_err_te_100)
     plt.savefig("results/subsample_100_estimated_err_test_"+str(numberIter)+".png")
     plt.close()
-
+    
     plt.plot(range(numberIter//monitorFreq+1), est_err_te_1000)
     plt.savefig("results/subsample_1000_estimated_err_test_"+str(numberIter)+".png")
     plt.close()
