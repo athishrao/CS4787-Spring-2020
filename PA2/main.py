2607506c19cb6cdd9f3fded314465c54e5006ac3
Athish Rahul Rao <athish@dhcp-ccc-6781.eduroam.cornell.edu>
2020-02-27 13:47:20 -0500

    PA-2

--- PA2/main.py
+++ PA2/main.py
@@ -1,12 +1,13 @@
 import os
-import numpy
+import numpy as np
 from numpy import random
 import scipy
 import matplotlib
 import mnist
 import pickle
 matplotlib.use('agg')
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from scipy.special import softmax
 
 mnist_data_directory = os.path.join(os.path.dirname(__file__), "data")
 
@@ -50,7 +51,15 @@ def load_MNIST_dataset():
 # returns   the average gradient of the regularized loss of the examples in vector ii with respect to the model parameters
 def multinomial_logreg_grad_i(Xs, Ys, ii, gamma, W):
     # TODO students should implement this
-
+    Xs = Xs.T
+    Ys = Ys.T
+    batchSize = len(ii)
+    totalLoss = 0
+    X_batch = Xs[ii]
+    Y_batch = Ys[ii]
+    yHat = (softmax(np.matmul(W, X_batch.T), axis = 0) - Y_batch.T)
+    ans = np.matmul(yHat, X_batch) + gamma*W
+    return ans/batchSize
 
 # compute the error of the classifier (SAME AS PROGRAMMING ASSIGNMENT 1)
 #
@@ -61,7 +70,14 @@ def multinomial_logreg_grad_i(Xs, Ys, ii, gamma, W):
 # returns   the model error as a fraction of incorrect labels (a number between 0 and 1)
 def multinomial_logreg_error(Xs, Ys, W):
     # TODO students should use their implementation from programming assignment 1
-
+    Ys = Ys.T
+    yHat = softmax(np.dot(W,Xs),axis=0).T
+    count = 0
+    for i in range(len(Ys)):
+        pred = np.argmax(yHat[i])
+        if (Ys[i,pred] != 1):
+            count += 1
+    return count/len(Ys)
 
 # ALGORITHM 1: run stochastic gradient descent on a multinomial logistic regression objective, with regularization
 #
@@ -74,9 +90,17 @@ def multinomial_logreg_error(Xs, Ys, W):
 # monitor_period  how frequently, in terms of iterations (not epochs) to output the parameter vector
 #
 # returns         a list of model parameters, one every "monitor_period" iterations
-def stochastic_gradient_descent(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_period):
+def stochastic_gradient_descent(Xs, Ys, gamma, W, alpha, num_epochs, monitor_period):
     # TODO students should implement this
-
+    T = num_epochs*Xs.shape[1]
+    params = []
+    for i in range(T):
+        if (i % monitor_period == 0):
+            params.append(W)
+        index = np.random.randint(0,Xs.shape[1])
+        W = W - alpha*(multinomial_logreg_grad_i(Xs, Ys, [index], gamma, W))
+    params.append(W)
+    return params
 
 # ALGORITHM 2: run stochastic gradient descent with sequential sampling order
 #
@@ -89,9 +113,16 @@ def stochastic_gradient_descent(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_pe
 # monitor_period  how frequently, in terms of iterations (not epochs) to output the parameter vector
 #
 # returns         a list of model parameters, one every "monitor_period" iterations
-def sgd_sequential_scan(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_period):
+def sgd_sequential_scan(Xs, Ys, gamma, W, alpha, num_epochs, monitor_period):
     # TODO students should implement this
-
+    params = []
+    for i in range(num_epochs):
+        for j in range(Xs.shape[1]):
+            if (j % monitor_period == 0):
+                params.append(W)
+            W = W - alpha*(multinomial_logreg_grad_i(Xs, Ys, [j], gamma, W))
+        params.append(W)
+    return params
 
 # ALGORITHM 3: run stochastic gradient descent with minibatching
 #
@@ -105,9 +136,19 @@ def sgd_sequential_scan(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_period):
 # monitor_period  how frequently, in terms of batches (not epochs) to output the parameter vector
 #
 # returns         a list of model parameters, one every "monitor_period" batches
-def sgd_minibatch(Xs, Ys, gamma, W0, alpha, B, num_epochs, monitor_period):
+def sgd_minibatch(Xs, Ys, gamma, W, alpha, B, num_epochs, monitor_period):
     # TODO students should implement this
-
+    params = []
+    T = num_epochs*Xs.shape[1]/N
+    for i in range(T):
+        if (i % monitor_period == 0):
+            params.append(W)
+        ii = []
+        for j in range(B):
+            ii.append(np.random.randint(0,Xs.shape[1]))
+        W = W - alpha*(multinomial_logreg_grad_i(Xs, Ys, ii, gamma, W))
+    params.append(W)
+    return params
 
 # ALGORITHM 4: run stochastic gradient descent with minibatching and sequential sampling order
 #
@@ -121,10 +162,40 @@ def sgd_minibatch(Xs, Ys, gamma, W0, alpha, B, num_epochs, monitor_period):
 # monitor_period  how frequently, in terms of batches (not epochs) to output the parameter vector
 #
 # returns         a list of model parameters, one every "monitor_period" batches
-def sgd_minibatch_sequential_scan(Xs, Ys, gamma, W0, alpha, B, num_epochs, monitor_period):
+def sgd_minibatch_sequential_scan(Xs, Ys, gamma, W, alpha, B, num_epochs, monitor_period):
     # TODO students should implement this
+    params = []
+    for t in range(num_epochs):
+        if (i % monitor_period == 0):
+            params.append(W)
+        ii = [(t*B + i) for i in range(B)]
+        W = W - alpha*(multinomial_logreg_grad_i(Xs, Ys, ii , gamma, W))
+    params.append(W)
+    return params
 
 
 if __name__ == "__main__":
     (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
     # TODO add code to produce figures
+    print("Shape of initial X:", Xs_tr.shape)
+    print("Shape of initial Y:", Ys_tr.shape)
+    DIVIDER = "#"*20
+    gamma = 0.0001
+    alpha = 0.001
+    num_epochs = 10
+    monitor_period = 1000
+    W = np.zeros([Ys_tr.shape[0], Xs_tr.shape[0]])
+
+    # params = stochastic_gradient_descent(Xs_tr, Ys_tr, gamma, W, alpha, num_epochs, monitor_period)
+    error = []
+    # error_np_te = 0
+
+    params = pickle.load(open("W.pickle", "rb"))
+    for w in params:
+        error.append(multinomial_logreg_error(Xs_tr, Ys_tr, w))
+    pickle.dump(params, open( "W.pickle", "wb" ) )
+
+    plt.plot(range(600+1), error)
+    plt.savefig("results/entire_ds_error_train_"+str(1)+".png")
+    plt.close()
+    # return
