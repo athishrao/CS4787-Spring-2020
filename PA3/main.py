ce9fcea555ad03a829936afb62f3e7be8519be79
Vedant Puri <vedantpuri13@gmail.com>
2020-03-26 23:21:18 -0400

    Implement hyperparam tuning routine

--- PA3/main.py
+++ PA3/main.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 import os
-import numpy as np
-from numpy import random
-import scipy
-import matplotlib
-import mnist
 import time
+import scipy
+import mnist
 import pickle
+import matplotlib
+import numpy as np
+import itertools as it
+from numpy import random
 
 matplotlib.use("agg")
 from matplotlib import pyplot as plt
 from scipy.special import softmax
+
 mnist_data_directory = os.path.join(os.path.dirname(__file__), "data")
 
 # TODO add any additional imports and global variables
+
 
 def load_MNIST_dataset():
     PICKLE_FILE = os.path.join(mnist_data_directory, "MNIST.pickle")
@@ -141,25 +144,29 @@ def gradient_descent(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_period):
 def multinomial_logreg_total_grad(Xs, Ys, gamma, W0):
     # TODO students should implement this
     # a starter solution using an average of the example gradients
-    (d,n) = Xs.shape
+    (d, n) = Xs.shape
     ret = 0
     # ----- NUMPY CODE
-    y_hat = softmax(np.dot(W0,Xs), axis=0)
+    y_hat = softmax(np.dot(W0, Xs), axis=0)
     del_L = np.dot(y_hat - Ys, Xs.T)
-    ret = del_L + gamma*W0
+    ret = del_L + gamma * W0
     return ret / n
+
+
 def multinomial_logreg_total_loss(Xs, Ys, gamma, W0):
     # TODO students should implement this
     # a starter solution using an average of the example gradients
-    (d,n) = Xs.shape
+    (d, n) = Xs.shape
     ret = 0
     # Numpy Code
-    y_hat = softmax(np.dot(W0,Xs), axis=0)
+    y_hat = softmax(np.dot(W0, Xs), axis=0)
     log_y_hat = -1 * np.log(y_hat)
     y_dot_y_hat = np.multiply(log_y_hat, Ys)
     L_y_y_hat = np.sum(y_dot_y_hat)
-    ret = L_y_y_hat + (gamma/2)*(np.linalg.norm(W0, 'fro'))**2
+    ret = L_y_y_hat + (gamma / 2) * (np.linalg.norm(W0, "fro")) ** 2
     return ret / n
+
+
 def gd_nesterov(Xs, Ys, gamma, W0, alpha, beta, num_epochs, monitor_period):
     # TODO students should implement this
     params = []
@@ -170,8 +177,6 @@ def gd_nesterov(Xs, Ys, gamma, W0, alpha, beta, num_epochs, monitor_period):
             params.append(W0)
         W0 = W0 - alpha * multinomial_logreg_total_grad(Xs, Ys, gamma, W0)
     params.append(W0)
-    error.append(multinomial_logreg_error(Xs, Ys, W0))
-    loss.append(multinomial_logreg_total_loss(Xs, Ys, gamma, W0))
     return params
 
 
@@ -268,16 +273,16 @@ def adam(Xs, Ys, gamma, W0, alpha, rho1, rho2, B, eps, num_epochs, monitor_perio
             for j in range(d):
                 s[j] = rho1 * s[j] + (1 - rho1) * g[j]
                 r[j] = rho2 * r[j] + (1 - rho2) * g[j] ** 2
-            s_cap = np.matrix([i/(1 - (rho1 ** t)) for i in s])
-            r_cap = np.matrix([i/(1 - (rho2 ** t)) for i in r])
+            s_cap = np.matrix([i / (1 - (rho1 ** t)) for i in s])
+            r_cap = np.matrix([i / (1 - (rho2 ** t)) for i in r])
             W0 = W0.T
             W_temp = []
             for j in range(d):
-                terms1 = W0[j].reshape(1,-1)
-                terms2 = ((alpha * s_cap[j]) / np.sqrt(r_cap[j] + eps)).reshape(1,-1)
+                terms1 = W0[j].reshape(1, -1)
+                terms2 = ((alpha * s_cap[j]) / np.sqrt(r_cap[j] + eps)).reshape(1, -1)
                 W_temp.append(terms1 - terms2)
             W_temp = np.array(W_temp)
-            W0 = W_temp.reshape(W0.shape[1],d)
+            W0 = W_temp.reshape(W0.shape[1], d)
     params.append(W0)
     return params
 
@@ -327,7 +332,74 @@ def get_error(Xs, Ys, params):
     return errors
 
 
-def generatePlot(weight, lossOrError, name, questionNumber, color='green'):
+def get_loss(Xs, Ys, params):
+    losses = []
+    for w in params:
+        losses.append(multinomial_logreg_total_loss(Xs, Ys, gamma, w))
+    return losses
+
+
+def print_config(config):
+    print("Current Configuration:")
+    print("~" * 15)
+    for k in config:
+        print(f"{k}: {config[k]}")
+    print("~" * 15)
+
+
+# Custom GridSearch
+# Returns only if tuning_basis has been provided (in which case it returns best config along with metrics)
+# Otherwise returns None
+def tune_hyperparams(
+    tr_data, te_data, hyper_params, algo_args, algo_fn, algo_id, tuning_basis=""
+):
+    train_x, train_y = tr_data
+    test_x, test_y = te_data
+    key_set = sorted(hyper_params)
+    configs = [
+        dict(zip(key_set, prod))
+        for prod in it.product(*(hyper_params[k] for k in key_set))
+    ]
+
+    print(DIVIDER)
+    print(DIVIDER)
+    print(f"Performing hyperparameter tuning for {algo_id} ...")
+    for config in configs:
+        print_config(config)
+        for k in config:
+            algo_args[k] = config[k]
+        params, _ = run_gd(train_x, train_y, "", algo_id, algo_fn, algo_args, True)
+        tr_err, te_err = (
+            min(get_error(train_x, train_y, params)),
+            min(get_error(test_x, test_y, params)),
+        )
+        tr_loss = min(get_loss(train_x, train_y, params))
+        print(f"Min. Training loss under current config is: {tr_loss}")
+        print(f"Min. Training error under current config is: {tr_err}")
+        print(f"Min. Testing error under current config is: {te_err}")
+        config["tr_loss"] = tr_loss
+        config["tr_err"] = tr_err
+        config["te_err"] = te_err
+    print(f"Tuning for {algo_id} complete.")
+
+    if tuning_basis:
+        best_dict = choose_best(tuning_basis, configs)
+        return best_dict
+
+    print(DIVIDER)
+    print(DIVIDER)
+
+
+# Custom Argmax
+# Choose best dict based on a certain key (metric)
+# Find either max or min
+def choose_best(metric, dict_list, find_max=False):
+    if not find_max:
+        return min(dict_list, key=lambda x: x[metric])
+    return max(dict_list, key=lambda x: x[metric])
+
+
+def generatePlot(weight, lossOrError, name, questionNumber, color="green"):
     figures_dir = "Figures/"
     if not os.path.isdir(figures_dir):
         print("Figures folder does not exist. Creating ...")
@@ -335,8 +407,8 @@ def generatePlot(weight, lossOrError, name, questionNumber, color='green'):
         print(f"Created {figures_dir}.")
     plt.plot(range(len(weight)), lossOrError)
     plt.xlabel("Number of Observations")
-    plt.ylabel(name+"_error")
-    plt.savefig(figures_dir + name + "_"+questionNumber+"_" + ".png")
+    plt.ylabel(name + "_error")
+    plt.savefig(figures_dir + name + "_" + questionNumber + "_" + ".png")
     plt.close()
 
 
@@ -352,7 +424,7 @@ if __name__ == "__main__":
 
     # --------------- PART 1 BEGINS ---------------
     gd_pickle_file = pickle_file_dir + "gd.pickle"
-    nesterov_pickle_file = pickle_file_dir + "nesterov.pickle"
+    nesterov_pickle_file = pickle_file_dir + "nesterov"
     gamma = 10 ** -4
     alpha = 1.0
     num_epochs = 100
@@ -395,11 +467,20 @@ if __name__ == "__main__":
     )
 
     # # TODO: Part 1.7 (Athish)
-    gd_tr_err, gd_te_err = (get_error(Xs_tr, Ys_tr, w_gd), get_error(Xs_te, Ys_te, w_gd))
+    gd_tr_err, gd_te_err = (
+        get_error(Xs_tr, Ys_tr, w_gd),
+        get_error(Xs_te, Ys_te, w_gd),
+    )
 
-    nesterov_gd_09_tr_err, nesterov_gd_09_te_err = (get_error(Xs_tr, Ys_tr, w_nest_09), get_error(Xs_te, Ys_te, w_nest_09))
+    nesterov_gd_09_tr_err, nesterov_gd_09_te_err = (
+        get_error(Xs_tr, Ys_tr, w_nest_09),
+        get_error(Xs_te, Ys_te, w_nest_09),
+    )
 
-    nesterov_gd_99_tr_err, nesterov_gd_99_te_err = (get_error(Xs_tr, Ys_tr, w_nest_099), get_error(Xs_te, Ys_te, w_nest_099))
+    nesterov_gd_99_tr_err, nesterov_gd_99_te_err = (
+        get_error(Xs_tr, Ys_tr, w_nest_099),
+        get_error(Xs_te, Ys_te, w_nest_099),
+    )
 
     # TODO: Part 1.8 (Athish)
     generatePlot(w_gd, gd_tr_err, "gd_tr_err", "1.8")
@@ -433,10 +514,22 @@ if __name__ == "__main__":
     # print(f"Average time for Basic GD for 5 total runs is: {gd_time}")
     # print(f"Average time for Nesterov GD for 5 total runs is: {nes_time}")
 
-    # TODO Part 1.10 (Unassigned)
-    # call hyperpar here -- TODO Vedant
-    alphas = [0.25, 0.5, 0.75]
-    betas = [0.5, 0.8, 0.925, 0.95]
+    # Part 1.10 (Unassigned)
+    hyperpar = {"alpha": [0.25, 0.5, 0.75]}
+    # ONLY PRINTS FOR NOW, RETURNS NOTHING BEC NO TUNING BASIS PASSED AS PARAM
+    tune_hyperparams(
+        (Xs_tr, Ys_tr), (Xs_te, Ys_te), hyperpar, gd_args, gradient_descent, "basic_gd"
+    )
+    hyperpar["beta"] = [0.5, 0.8, 0.925, 0.95]
+    # ONLY PRINTS FOR NOW, RETURNS NOTHING BEC NO TUNING BASIS PASSED AS PARAM
+    tune_hyperparams(
+        (Xs_tr, Ys_tr),
+        (Xs_te, Ys_te),
+        hyperpar,
+        nesterov_gd_args,
+        gd_nesterov,
+        "nesterov_gd",
+    )
 
     # --------------- PART 2 BEGINS ---------------
 
@@ -462,7 +555,7 @@ if __name__ == "__main__":
         sgd_args,
     )
 
-    momen_sgd_pickle_file = pickle_file_dir + "sgd_momentum.pickle"
+    momen_sgd_pickle_file = pickle_file_dir + "sgd_momentum"
     momentum_sgd_args = dict(sgd_args)
 
     momentum_sgd_args["beta"] = 0.9
@@ -485,14 +578,23 @@ if __name__ == "__main__":
         momentum_sgd_args,
     )
 
-    # # TODO: Part 2.4 (Athish)
-    sgd_tr_err, sgd_te_err = (get_error(Xs_tr, Ys_tr, w_sgd), get_error(Xs_te, Ys_te, w_sgd))
+    # Part 2.4 (Athish)
+    sgd_tr_err, sgd_te_err = (
+        get_error(Xs_tr, Ys_tr, w_sgd),
+        get_error(Xs_te, Ys_te, w_sgd),
+    )
 
-    sgd_momen_09_tr_err, sgd_momen_09_te_err = (get_error(Xs_tr, Ys_tr, w_sgd_momen_09), get_error(Xs_te, Ys_te, w_sgd_momen_09))
+    sgd_momen_09_tr_err, sgd_momen_09_te_err = (
+        get_error(Xs_tr, Ys_tr, w_sgd_momen_09),
+        get_error(Xs_te, Ys_te, w_sgd_momen_09),
+    )
 
-    sgd_momen_99_tr_err, sgd_momen_99_te_err = (get_error(Xs_tr, Ys_tr, w_sgd_momen_099), get_error(Xs_te, Ys_te, w_sgd_momen_099))
+    sgd_momen_99_tr_err, sgd_momen_99_te_err = (
+        get_error(Xs_tr, Ys_tr, w_sgd_momen_099),
+        get_error(Xs_te, Ys_te, w_sgd_momen_099),
+    )
 
-    # TODO: Part 2.5 (Athish)
+    # Part 2.5 (Athish)
     generatePlot(w_sgd, sgd_tr_err, "sgd_tr_err", "2.5")
     generatePlot(w_sgd, sgd_te_err, "sgd_te_err", "2.5")
 
@@ -529,10 +631,27 @@ if __name__ == "__main__":
     # print(f"Average time for Basic SGD for 5 total runs is: {sgd_time}")
     # print(f"Average time for Momentum SGD for 5 total runs is: {sgd_momen_time}")
 
-    # TODO: Part 2.7 (Unassigned)
-    # call hyperpar here -- TODO Vedant
-    alphas = [0.25, 0.5, 0.75]
-    betas = [0.5, 0.8, 0.925, 0.95]
+    # Part 2.7 (Unassigned)
+    hyperpar = {"alpha": [0.25, 0.5, 0.75]}
+    # ONLY PRINTS FOR NOW, RETURNS NOTHING BEC NO TUNING BASIS PASSED AS PARAM
+    tune_hyperparams(
+        (Xs_tr, Ys_tr),
+        (Xs_te, Ys_te),
+        hyperpar,
+        sgd_args,
+        sgd_minibatch_sequential_scan,
+        "basic_sgd",
+    )
+    hyperpar["beta"] = [0.5, 0.8, 0.925, 0.95]
+    # ONLY PRINTS FOR NOW, RETURNS NOTHING BEC NO TUNING BASIS PASSED AS PARAM
+    tune_hyperparams(
+        (Xs_tr, Ys_tr),
+        (Xs_te, Ys_te),
+        hyperpar,
+        momentum_sgd_args,
+        sgd_mss_with_momentum,
+        "momen_sgd",
+    )
 
     # --------------- PART 3 BEGINS ---------------
 
@@ -550,9 +669,15 @@ if __name__ == "__main__":
     )
 
     # TODO: Part 3.3 (Athish)
-    sgd_tr_err, sgd_te_err = (get_error(Xs_tr, Ys_tr, w_sgd), get_error(Xs_te, Ys_te, w_sgd))
+    sgd_tr_err, sgd_te_err = (
+        get_error(Xs_tr, Ys_tr, w_sgd),
+        get_error(Xs_te, Ys_te, w_sgd),
+    )
 
-    sgd_adam_tr_err, sgd_adam_te_err = (get_error(Xs_tr, Ys_tr, w_sgd_adam), get_error(Xs_te, Ys_te, w_sgd_adam))
+    sgd_adam_tr_err, sgd_adam_te_err = (
+        get_error(Xs_tr, Ys_tr, w_sgd_adam),
+        get_error(Xs_te, Ys_te, w_sgd_adam),
+    )
 
     # TODO: Part 3.4 (Athish)
     generatePlot(w_sgd, sgd_tr_err, "sgd_tr_err", "3.3")
@@ -573,7 +698,12 @@ if __name__ == "__main__":
     # print(f"Average time for Adam SGD for 5 total runs is: {sgd_adam_time}")
 
     # TODO: Part 3.6 (Unassigned)
-    # call hyperpar here -- TODO Vedant
-    alphas = [0.25, 0.5, 0.75]
-    rho1 = [0.5, 0.8, 0.925, 0.95]
-    rho2 = [0.5, 0.8, 0.925, 0.95]
+    hyperpar = {
+        "alpha": [0.25, 0.5, 0.75],
+        "rho1": [0.5, 0.8, 0.925, 0.95],
+        "rho2": [0.5, 0.8, 0.925, 0.95],
+    }
+    # ONLY PRINTS FOR NOW, RETURNS NOTHING BEC NO TUNING BASIS PASSED AS PARAM
+    tune_hyperparams(
+        (Xs_tr, Ys_tr), (Xs_te, Ys_te), hyperpar, adam_sgd_args, adam, "adam_sgd"
+    )
