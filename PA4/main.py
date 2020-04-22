b7e0b5c64ce6061a3abbe10dbce9fdfc90ffff6c
Vedant Puri <vedantpuri13@gmail.com>
2020-04-22 14:35:26 -0400

    Add Experiments except Part 2

--- PA4/main.py
+++ PA4/main.py
@@ -8,34 +8,26 @@ import mnist
 import pickle
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
+
 # matplotlib.use('agg')
 from matplotlib import pyplot as plt
 
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
+
 mnist = tf.keras.datasets.mnist
 
 if tf.__version__ != "2.1.0":
-	print("-" * 40)
-	print("**** warning: this assignment is tested on TensorFlow 2.1.0 or newer.")
-	print("****         if you run into issues, please check that you are updated to the latest stable TensorFlow version")
-	print("-" * 40)
+    print("-" * 40)
+    print("**** warning: this assignment is tested on TensorFlow 2.1.0 or newer.")
+    print(
+        "****         if you run into issues, please check that you are updated to the latest stable TensorFlow version"
+    )
+    print("-" * 40)
 
-### hyperparameter settings and other constants
-batch_size = 128
-num_classes = 10
-epochs = 10
-mnist_input_shape = (28, 28, 1)
-d1 = 1024
-d2 = 256
-alpha = 0.1
-beta = 0.9
-alpha_adam = 0.001
-rho1 = 0.99
-rho2 = 0.999
-### end hyperparameter settings
-
+# Constants
+validation_percentage = 0.1
 
 # load the MNIST dataset using TensorFlow/Keras
 def load_MNIST_dataset():
@@ -46,7 +38,7 @@ def load_MNIST_dataset():
     # plt.show()
     Xs_tr = Xs_tr / 255.0
     Xs_te = Xs_te / 255.0
-    Xs_tr = Xs_tr.reshape(Xs_tr.shape[0], 28, 28, 1) # 28 rows, 28 columns, 1 channel
+    Xs_tr = Xs_tr.reshape(Xs_tr.shape[0], 28, 28, 1)  # 28 rows, 28 columns, 1 channel
     Xs_te = Xs_te.reshape(Xs_te.shape[0], 28, 28, 1)
     return (Xs_tr, Ys_tr, Xs_te, Ys_te)
 
@@ -80,21 +72,27 @@ def evaluate_model(Xs, Ys, model):
 def train_fully_connected_sgd(Xs, Ys, d1, d2, alpha, beta, B, Epochs):
     # TODO students should implement this
 
-    Xs = Xs.reshape(60000, 784).astype('float64')
-    Ys = Ys.astype('float64')
+    Xs = Xs.reshape(60000, 784).astype("float64")
+    Ys = Ys.astype("float64")
 
     model = models.Sequential()
-    model.add(layers.Dense(d1, activation='relu', name='dense_1'))
-    model.add(layers.Dense(d2, activation='relu', name='dense_2'))
-    model.add(layers.Dense(10, activation='softmax', name='predictions'))
+    model.add(layers.Dense(d1, activation="relu", name="dense_1"))
+    model.add(layers.Dense(d2, activation="relu", name="dense_2"))
+    model.add(layers.Dense(10, activation="softmax", name="predictions"))
 
-    opt = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta, nesterov=False, name='SGD')
+    opt = tf.keras.optimizers.SGD(
+        learning_rate=alpha, momentum=beta, nesterov=False, name="SGD"
+    )
     lossFunc = keras.losses.SparseCategoricalCrossentropy()
 
-    model.compile(optimizer=opt,loss=lossFunc,metrics=['acc'])
-    history = model.fit(Xs, Ys,batch_size=B,epochs=Epochs)
-    
+    model.compile(optimizer=opt, loss=lossFunc, metrics=["acc"])
+    history = model.fit(
+        Xs, Ys, batch_size=B, epochs=Epochs, validation_split=validation_percentage
+    )
+
     return model, history
+
+
 # train a fully connected two-hidden-layer neural network on MNIST data using Adam, and print the usual output from TF
 #
 # Xs        training examples
@@ -112,22 +110,33 @@ def train_fully_connected_sgd(Xs, Ys, d1, d2, alpha, beta, B, Epochs):
 #   history     the history of training returned by model.fit (should be of type tensorflow.python.keras.callbacks.History)
 def train_fully_connected_adam(Xs, Ys, d1, d2, alpha, rho1, rho2, B, Epochs):
     # TODO students should implement this
-    
-    Xs = Xs.reshape(60000, 784).astype('float64')
-    Ys = Ys.astype('float64')
+
+    Xs = Xs.reshape(60000, 784).astype("float64")
+    Ys = Ys.astype("float64")
 
     model = models.Sequential()
-    model.add(layers.Dense(d1, activation='relu', name='dense_1'))
-    model.add(layers.Dense(d2, activation='relu', name='dense_2'))
-    model.add(layers.Dense(10, activation='softmax', name='predictions'))
+    model.add(layers.Dense(d1, activation="relu", name="dense_1"))
+    model.add(layers.Dense(d2, activation="relu", name="dense_2"))
+    model.add(layers.Dense(10, activation="softmax", name="predictions"))
 
-    opt = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=rho1, beta_2=rho2, epsilon=1e-07, amsgrad=False, name='Adam')
+    opt = tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=rho1,
+        beta_2=rho2,
+        epsilon=1e-07,
+        amsgrad=False,
+        name="Adam",
+    )
     lossFunc = keras.losses.SparseCategoricalCrossentropy()
 
-    model.compile(optimizer=opt,loss=lossFunc,metrics=['acc'])
-    history = model.fit(Xs, Ys,batch_size=B,epochs=Epochs)
-    
+    model.compile(optimizer=opt, loss=lossFunc, metrics=["acc"])
+    history = model.fit(
+        Xs, Ys, batch_size=B, epochs=Epochs, validation_split=validation_percentage
+    )
+
     return model, history
+
+
 # train a fully connected two-hidden-layer neural network with Batch Normalization on MNIST data using SGD, and print the usual output from TF
 #
 # Xs        training examples
@@ -145,23 +154,29 @@ def train_fully_connected_adam(Xs, Ys, d1, d2, alpha, rho1, rho2, B, Epochs):
 def train_fully_connected_bn_sgd(Xs, Ys, d1, d2, alpha, beta, B, Epochs):
     # TODO students should implement this
 
-    Xs = Xs.reshape(60000, 784).astype('float64')
-    Ys = Ys.astype('float64')
+    Xs = Xs.reshape(60000, 784).astype("float64")
+    Ys = Ys.astype("float64")
 
     model = models.Sequential()
-    model.add(layers.Dense(d1, activation='relu', name='dense_1'))
+    model.add(layers.Dense(d1, activation="relu", name="dense_1"))
     model.add(layers.BatchNormalization(axis=-1, momentum=beta))
-    model.add(layers.Dense(d2, activation='relu', name='dense_2'))
+    model.add(layers.Dense(d2, activation="relu", name="dense_2"))
     model.add(layers.BatchNormalization(axis=-1, momentum=beta))
-    model.add(layers.Dense(10, activation='softmax', name='predictions'))
+    model.add(layers.Dense(10, activation="softmax", name="predictions"))
 
-    opt = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta, nesterov=False, name='SGD')
+    opt = tf.keras.optimizers.SGD(
+        learning_rate=alpha, momentum=beta, nesterov=False, name="SGD"
+    )
     lossFunc = keras.losses.SparseCategoricalCrossentropy()
-    
-    model.compile(optimizer=opt,loss=lossFunc,metrics=['acc'])
-    history = model.fit(Xs, Ys,batch_size=B,epochs=Epochs)
+
+    model.compile(optimizer=opt, loss=lossFunc, metrics=["acc"])
+    history = model.fit(
+        Xs, Ys, batch_size=B, epochs=Epochs, validation_split=validation_percentage
+    )
 
     return model, history
+
+
 # train a convolutional neural network on MNIST data using SGD, and print the usual output from TF
 #
 # Xs        training examples
@@ -178,26 +193,86 @@ def train_fully_connected_bn_sgd(Xs, Ys, d1, d2, alpha, beta, B, Epochs):
 def train_CNN_sgd(Xs, Ys, alpha, rho1, rho2, B, Epochs):
     # TODO students should implement this
     model = models.Sequential()
-    model.add(layers.Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)))
+    model.add(
+        layers.Conv2D(
+            32, kernel_size=(5, 5), activation="relu", input_shape=(28, 28, 1)
+        )
+    )
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, kernel_size=(5, 5), activation='relu'))
+    model.add(layers.Conv2D(64, kernel_size=(5, 5), activation="relu"))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(10, activation='softmax'))
+    model.add(layers.Dense(512, activation="relu"))
+    model.add(layers.Dense(10, activation="softmax"))
 
-    opt = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta, nesterov=False, name='SGD')
+    opt = tf.keras.optimizers.SGD(
+        learning_rate=alpha, momentum=beta, nesterov=False, name="SGD"
+    )
     lossFunc = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    
-    model.compile(optimizer=opt,loss=lossFunc,metrics=['acc'])
-    history = model.fit(Xs, Ys,batch_size=B,epochs=Epochs)
+
+    model.compile(optimizer=opt, loss=lossFunc, metrics=["acc"])
+    history = model.fit(
+        Xs, Ys, batch_size=B, epochs=Epochs, validation_split=validation_percentage
+    )
 
     return model, history
 
-if __name__ == "__main__":
-    (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
-    # TODO students should add code to generate plots here
 
-    # train_fully_connected_sgd(Xs_tr, Ys_tr, d1, d2, alpha, beta, batch_size, epochs)
-    # train_fully_connected_adam(Xs_tr, Ys_tr, d1, d2, alpha_adam, rho1, rho2, batch_size, epochs)
-    # train_fully_connected_bn_sgd(Xs_tr, Ys_tr, d1, d2, alpha, beta, batch_size, epochs)
-    train_CNN_sgd(Xs_tr, Ys_tr, alpha, rho1, rho2, batch_size, epochs)
+def run_algo(algorithm_identifier, algo_fn, algo_args, X_te, Y_te):
+    print(f"Running Algorithm {algorithm_identifier} ...")
+    start = time.time()
+    model, history = algo_fn(**algo_args)
+    end = time.time()
+    time_taken = end - start
+    print(f"Algorithm {algorithm_identifier} complete. Time taken: {time_taken}")
+
+    print(f"Testing Algorithm {algorithm_identifier} ...")
+    te_l, te_acc = evaluate_model(X_te, Y_te, model)
+    print(f"Testing {algorithm_identifier} complete.")
+
+    return model, history, te_l, te_acc, time_taken
+
+
+if __name__ == "__main__":
+    # (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
+
+    DIVIDER = "#" * 20
+    basic_args = {
+        "Xs": Xs_tr,
+        "Ys": Ys_tr,
+        "d1": 1024,
+        "d2": 256,
+        "alpha": 0.1,
+        "Epochs": 10,
+        "B": 128,
+    }
+    sgd_no_momen_args = copy.copy(basic_args)
+    sgd_no_momen_args["beta"] = 0.0
+
+    sgd_momen_args = copy.copy(sgd_no_momen_args)
+    sgd_momen_args["beta"] = 0.9
+
+    adam_args = copy.copy(basic_args)
+    adam_args["alpha"] = 0.001
+    adam_args["rho1"] = 0.99
+    adam_args["rho2"] = 0.999
+
+    sgd_bn_args = copy.copy(basic_args)
+    sgd_bn_args["alpha"] = 0.001
+    sgd_bn_args["beta"] = 0.9
+
+    algorithms = {
+        "sgd_no_momen": (train_fully_connected_sgd, sgd_no_momen_args),
+        "sgd_momen": (train_fully_connected_sgd, sgd_momen_args),
+        "adam": (train_fully_connected_adam, adam_args),
+        "sgd_bn": (train_fully_connected_bn_sgd, sgd_bn_args),
+        "cnn": (train_CNN_sgd, adam_args),
+    }
+
+    # Experiments
+    for algorithm in algorithms:
+        model, history, te_l, te_acc, time_taken = run_algo(
+            algorithm, algorithms[algorithm][0], algorithms[algorithm][1], X_te, Y_te
+        )
+        # Generate Plots & Report here: Take a dict and unpack dict in plotting
+        # te_l and te_acc are scalars, create an arr of len = len(x-axis marks) where each elem in the arr is the same value
+        print(f"Time taken to run {algorithm}: {time_taken}")
